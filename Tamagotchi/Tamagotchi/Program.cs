08e4ad098f8af626034e1ef37150dfad9ce9e207
Gabriele Felice <gabriele_felice@hotmail.com>
2022-09-24 08:51:23 -0300

    1/7

---
+++ Tamagotchi/Tamagotchi/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using RestSharp;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("7DaysOfCode - C# 1/7: Conexão com uma API Rest");
+
+        var url = "https://pokeapi.co/api/v2/pokemon";
+        var client = new RestClient(url);
+        var request = new RestRequest(url, Method.Get);
+        RestResponse response = await client.ExecuteAsync(request);
+
+        Console.WriteLine(response.Content);
+    }
+}
