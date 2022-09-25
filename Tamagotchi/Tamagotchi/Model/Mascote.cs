62a1163b279b4552e1cc76479dee57eaa0d50c00
Gabriele Felice <gabriele_felice@hotmail.com>
2022-09-25 09:11:27 -0300

    2/7

---
+++ Tamagotchi/Tamagotchi/Model/Mascote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+    public class Mascote
+    {
+        public List<Abilities> Habilidades { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public string Nome { get; set; }
+        public int Alimentacao { get; set; }
+        public int Humor { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
--- Tamagotchi/Tamagotchi/Program.cs
+++ Tamagotchi/Tamagotchi/Program.cs
@@ -1,17 +1,40 @@
-﻿
-using RestSharp;
+﻿using RestSharp;
+using System;
+using System.Text.Json;
+using Tamagotchi;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
-        Console.WriteLine("7DaysOfCode - C# 1/7: Conexão com uma API Rest");
+        Console.WriteLine("7DaysOfCode - C# 2/7: Conexão com uma API Rest \r\n");
+        Console.WriteLine("Qual espécie: ");
+        string especie = Console.ReadLine();
 
-        var url = "https://pokeapi.co/api/v2/pokemon";
+        var url = $"https://pokeapi.co/api/v2/pokemon/{especie}";
         var client = new RestClient(url);
         var request = new RestRequest(url, Method.Get);
         RestResponse response = await client.ExecuteAsync(request);
 
-        Console.WriteLine(response.Content);
+        Console.WriteLine("\r\n_______________________________________\r\n\r\n");
+
+        if (response.IsSuccessful)
+        {
+            var pokemon = JsonSerializer.Deserialize<Pokemon>(response.Content);
+
+            Console.WriteLine($"Nome Pokemon: {pokemon.name}");
+            Console.WriteLine($"Altura: {pokemon.height}");
+            Console.WriteLine($"Peso: {pokemon.weight}");
+            Console.WriteLine($"Habilitades: ");
+
+            foreach (var item in pokemon.abilities)
+            {
+                Console.WriteLine($"    - {item.ability.name}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Não Encontrado");
+        }
     }
 }
